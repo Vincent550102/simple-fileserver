49bb569bedac951f36426fe9ccb38d4296f53864
Vincent550102 <5020559@gmail.com>
2023-08-08 06:43:33 +0000

    update upload file url resp

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from werkzeug.utils import secure_filename
 import os
 
 UPLOAD_FOLDER = 'tmp'  # 替換成你想要上傳文件保存的地方
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.abspath(UPLOAD_FOLDER)
@@ -12,26 +12,26 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'File uploaded successfully!'
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
+    <form action="/files" method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
     '''
 
-@app.route('/files', methods=['GET'])
-def list_files():
+@app.route('/files', methods=['GET', 'POST'])
+def list_upload_files():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return jsonify({"message": "File uploaded successfully.", "url": request.url_root+"files/"+filename}), 200
     files = os.listdir(app.config['UPLOAD_FOLDER'])
     return jsonify(files)
 
