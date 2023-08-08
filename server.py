ddd39e38159d4868093e62c606f6d4d83d2b5535
Vincent550102 <5020559@gmail.com>
2023-08-08 01:18:00 +0000

    update delete file feature

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ UPLOAD_FOLDER = 'tmp'  # 替換成你想要上傳文件保存的地方
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = os.path.abspath(UPLOAD_FOLDER)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -41,6 +41,20 @@ def get_file(filename):
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
     else:
         abort(404, description="File not found")
+
+@app.route('/files/<filename>', methods=['DELETE'])
+def delete_file(filename):
+    file_path = os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    if not file_path.startswith(app.config['UPLOAD_FOLDER']):
+        abort(403, description="Unauthorized request")
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return jsonify({"message": f"'{filename}' has been deleted."}), 200
+    else:
+        abort(404, description=f"'{filename}' not found.")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
